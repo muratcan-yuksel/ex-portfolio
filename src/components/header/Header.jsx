bbdb6d07f5b54f8eabe7fae759a2608fff715e4a
muratcan-yuksel <muratcanyukselpro@gmail.com>
2022-04-09 18:51:01 +0300

    fallback

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -5,8 +5,8 @@ const Header = () => {
   return (
     <div className="header d-flex justify-content-center align-items-center">
       <div className="whiteBox d-flex flex-column justify-content-center align-items-center">
-        <span className="name">Murat Can Yüksel</span>
-        <div className="occupation">
+        <span className="name text-center">Murat Can Yüksel</span>
+        <div className="occupation text-center">
           Web <span className="three">3</span> developer{" "}
         </div>
         <div className="stack">ReactJS Solidity</div>
