0ed240e4615b82fcad67cfd0dbdb4ad28ba7f84d
muratcan-yuksel <muratcanyukselpro@gmail.com>
2022-04-09 18:14:47 +0300

    add import 'bootstrap/dist/css/bootstrap.min.css'

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,7 +2,11 @@ import React from "react";
 import "./header.css";
 
 const Header = () => {
-  return <div className="header">Header</div>;
+  return (
+    <div className="header d-flex justify-content-center align-items-center">
+      <div className="whiteBox"></div>
+    </div>
+  );
 };
 
 export default Header;
