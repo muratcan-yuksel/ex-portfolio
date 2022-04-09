b2a57c17e5be09604deaad3979334b219d5823de
muratcan-yuksel <muratcanyukselpro@gmail.com>
2022-04-09 18:45:01 +0300

    adding header texts

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -4,7 +4,13 @@ import "./header.css";
 const Header = () => {
   return (
     <div className="header d-flex justify-content-center align-items-center">
-      <div className="whiteBox"></div>
+      <div className="whiteBox d-flex flex-column justify-content-center align-items-center">
+        <span className="name">Murat Can Yüksel</span>
+        <div className="occupation">
+          Web <span className="three">3</span> developer{" "}
+        </div>
+        <div className="stack">ReactJS Solidity</div>
+      </div>
     </div>
   );
 };
