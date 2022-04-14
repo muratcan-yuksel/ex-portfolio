412ee678e7ec8a260659b2ecc6b618ba11b77d71
muratcan-yuksel <muratcanyukselpro@gmail.com>
2022-04-14 05:27:22 +0300

    trying to deploy

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "https://cdnjs.cloudflare.com/ajax/libs/gsap/1.14.2/TweenMax.min.js";
 
 ReactDOM.render(
   <React.StrictMode>
