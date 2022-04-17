6b9cb2f36e7ec6630128f8f6cb6c270db231e03d
muratcan-yuksel <muratcanyukselpro@gmail.com>
2022-04-17 16:25:11 +0300

    add glassmorphism and then, fade effect to three

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -6,16 +6,20 @@ import Dots from "./dots-wrapper/Dots";
 const Header = () => {
   return (
     <div className="header d-flex justify-content-center align-items-center">
-      <div className="whiteBox d-flex flex-column justify-content-center align-items-center">
+      <div className="whiteBox d-flex flex-column justify-content-start align-items-start">
         <div className="name text-center">Murat Can Yüksel</div>
         <div className="occupation text-center">
           Web <span className="three">3</span> developer{" "}
         </div>
         <div className="animatedLine"></div>
         <div className="stack">ReactJS Solidity</div>
-
         {/* <BsTriangle className="tri" /> */}
-        {/* <Dots /> */}
+        {/* <Dots /> */}{" "}
+        {/* <img
+          src="https://assets.codepen.io/2621168/abstract.jpg"
+          alt="abstract background"
+        ></img> */}
+        <div className="item"></div>
       </div>
     </div>
   );
