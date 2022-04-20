25b11a9d4c218bfb692b259ea1d235c1eefd0d05
muratcan-yuksel <muratcanyukselpro@gmail.com>
2022-04-20 10:44:19 +0300

    fallback

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -3,31 +3,10 @@ import "./about.css";
 
 const About = () => {
   return (
-    <div className="about">
-      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsa aliquid ea
-      impedit aut veritatis vitae exercitationem excepturi in id tenetur itaque
-      recusandae, illo quia repudiandae? Similique aliquam totam et error. Lorem
-      ipsum dolor sit amet consectetur adipisicing elit. Impedit aut tempore
-      fuga omnis architecto odio, sapiente ratione ullam, ipsum sed neque
-      perspiciatis ut iure nobis inventore, soluta cupiditate consectetur ad!
-      <div>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Unde tempora
-        commodi ipsa nostrum consectetur. Minus repudiandae numquam nulla totam.
-        Deserunt quis non, temporibus voluptate animi quae hic sit repellat
-        nostrum?
-      </div>
-      <div>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Unde tempora
-        commodi ipsa nostrum consectetur. Minus repudiandae numquam nulla totam.
-        Deserunt quis non, temporibus voluptate animi quae hic sit repellat
-        nostrum?
-      </div>
-      <div>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Unde tempora
-        commodi ipsa nostrum consectetur. Minus repudiandae numquam nulla totam.
-        Deserunt quis non, temporibus voluptate animi quae hic sit repellat
-        nostrum?
-      </div>
+    <div className="aboutComponent">
+      <div className="aboutMe"></div>
+      <div className="techStack"></div>
+      <div className="glass"></div>
     </div>
   );
 };
