56a137af4d8dd43221ba4033614f18bf6b83f36b
muratcan-yuksel <muratcanyukselpro@gmail.com>
2022-04-20 10:16:42 +0300

    start about section

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "./components/header/Header";
+import About from "./components/about/About";
 import "./App.css";
 const App = () => {
   return (
     <div className="App">
       <Header />
+      <About />
     </div>
   );
 };
---
+++ src/components/about/About.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import "./about.css";
+
+const About = () => {
+  return (
+    <div className="about">
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsa aliquid ea
+      impedit aut veritatis vitae exercitationem excepturi in id tenetur itaque
+      recusandae, illo quia repudiandae? Similique aliquam totam et error. Lorem
+      ipsum dolor sit amet consectetur adipisicing elit. Impedit aut tempore
+      fuga omnis architecto odio, sapiente ratione ullam, ipsum sed neque
+      perspiciatis ut iure nobis inventore, soluta cupiditate consectetur ad!
+      <div>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Unde tempora
+        commodi ipsa nostrum consectetur. Minus repudiandae numquam nulla totam.
+        Deserunt quis non, temporibus voluptate animi quae hic sit repellat
+        nostrum?
+      </div>
+      <div>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Unde tempora
+        commodi ipsa nostrum consectetur. Minus repudiandae numquam nulla totam.
+        Deserunt quis non, temporibus voluptate animi quae hic sit repellat
+        nostrum?
+      </div>
+      <div>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Unde tempora
+        commodi ipsa nostrum consectetur. Minus repudiandae numquam nulla totam.
+        Deserunt quis non, temporibus voluptate animi quae hic sit repellat
+        nostrum?
+      </div>
+    </div>
+  );
+};
+
+export default About;
