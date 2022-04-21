c23b149381c0fd1fa2829e054e3af963b34a809a
muratcan-yuksel <muratcanyukselpro@gmail.com>
2022-04-21 16:33:18 +0300

    add parallax one

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Header from "./components/header/Header";
 import About from "./components/about/About";
+import ParallaxOne from "./components/parallaxOne/ParallaxOne";
 import "./App.css";
 const App = () => {
   return (
     <div className="App">
       <Header />
+      <ParallaxOne />
       <About />
     </div>
   );
---
+++ src/components/parallaxOne/ParallaxOne.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./parallaxOne.css";
+import image from "../../assets/images/parallax1.jpg";
+
+const ParallaxOne = () => {
+  return (
+    <div className="parallaxOne">
+      {/* <img src={image} alt="" className="bg" /> */}
+      <div className="bg"></div>
+    </div>
+  );
+};
+
+export default ParallaxOne;
