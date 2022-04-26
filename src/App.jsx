47fd39760f814f840bc11b03e9ec74093a6ce739
muratcan-yuksel <muratcanyukselpro@gmail.com>
2022-04-26 21:41:10 +0300

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
-import React from "react";
+import { React, useEffect } from "react";
 import Header from "./components/header/Header";
 import About from "./components/about/About";
 import ParallaxOne from "./components/parallaxOne/ParallaxOne";
 import "./App.css";
 const App = () => {
+  useEffect(() => {
+    // document.body.style.backgroundColor = "black";
+    document.body.style.overflowX = "hidden";
+  }, []);
   return (
     <div className="App">
       <Header />
