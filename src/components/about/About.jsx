f23e5d4b27f28360c426f1155e2319b1d3f2cf5b
muratcan-yuksel <muratcanyukselpro@gmail.com>
2022-04-26 21:47:06 +0300

    .

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -6,30 +6,16 @@ const About = () => {
     <div className="aboutComponent">
       <div className="texts d-flex flex-column justify-content-center align-items-center flex-md-row  justify-content-md-between">
         <div className="aboutMe">
-          <h2 className="title">About Me</h2>
+          <h2 className="title">
+            {" "}
+            const aboutMe= &#123; <br /> currentPosition: "Front-end developer",{" "}
+            <br />
+            cupiditate atque! <br />
+            &#125;
+          </h2>
         </div>
         <div className="techStack">
-          <h2 className="title">
-            My Tech Stack Lorem ipsum dolor sit amet consectetur adipisicing
-            elit. Molestiae dignissimos accusantium tempora perferendis,
-            reiciendis saepe, necessitatibus illum repellendus repellat maxime
-            debitis ipsum fugiat fugit doloremque dolorum inventore. Quisquam,
-            obcaecati ullam. Nesciunt natus mollitia itaque! Doloribus dolores
-            culpa sunt esse omnis, beatae aut minima nam, inventore nihil iusto
-            unde. Velit tempore eveniet fugiat, at sapiente harum id eius odit
-            quam. Animi. Unde qui rerum accusamus recusandae doloribus rem
-            asperiores illo, modi dignissimos dolores inventore, ut aspernatur!
-            Officiis, nihil! Dolorum ducimus nemo temporibus dolorem tempore
-            error. Ullam accusantium impedit explicabo at numquam. Consequuntur
-            vitae, esse, ducimus illo quisquam quos perferendis saepe tenetur
-            adipisci assumenda veniam, eligendi vel nam atque sed nisi! Quam
-            deserunt exercitationem ipsam quia expedita. Itaque corporis odit
-            eius expedita. Velit consequuntur obcaecati, explicabo voluptatum
-            facere exercitationem amet eligendi nostrum, aspernatur vel
-            molestias maiores dolorum. Error dolores explicabo rerum non, nihil
-            eum quisquam doloribus deserunt dignissimos, temporibus amet
-            corporis quae.
-          </h2>
+          <h2 className="title">My Tech Stack</h2>
         </div>
       </div>
       {/* <div className="glass"></div> */}
