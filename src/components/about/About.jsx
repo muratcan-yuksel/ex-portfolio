044045b59a762f2bef2b4bc25c0d2a19a5fe3e93
muratcan-yuksel <muratcanyukselpro@gmail.com>
2022-05-08 18:35:04 +0300

    fallback

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -26,12 +26,28 @@ const About = () => {
             <p className="para">I'm fluent in English, French, and Turkish.</p>
             <p className="para">
               As a lifelong learner, these days I'm interested heavily with the
-              web3 space, studying Solidity daily.
+              web3 space.
             </p>
           </div>
         </div>
         <div className="techStack">
           <h2 className="title">My Tech Stack</h2>
+          <div className="glass">
+            <ul>
+              <li className="stack">HTML/CSS/Javacript</li>
+              <li className="stack">ReactJs</li>
+              <li className="stack">VueJS</li>
+              <li className="stack">Solidity</li>
+              <li className="stack">Hardhat/EthersJs</li>
+              <li className="stack">SASS</li>
+              <li className="stack">Bootstrap</li>
+              <li className="stack">Google Firebase/Cloud Firestore</li>
+              <li className="stack">MongoDB</li>
+              <li className="stack">ChartJS/Apexcharts</li>
+              <li className="stack">Webpack</li>
+              <li className="stack">Git</li>
+            </ul>
+          </div>
         </div>
       </div>
     </div>
