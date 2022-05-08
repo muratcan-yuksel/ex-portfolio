f112908c09ff74ed30d5668812388ebdedd14249
muratcan-yuksel <muratcanyukselpro@gmail.com>
2022-05-08 18:17:14 +0300

    start adding about me

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -7,13 +7,33 @@ const About = () => {
       <div className="texts d-flex flex-column justify-content-center align-items-center flex-md-row  justify-content-md-between">
         <div className="aboutMe">
           <h2 className="title">About Me</h2>
-          <div className="para">Hey!</div>
+          <div className="glass">
+            <div className=" bigPara">
+              Hey! My name is Murat Can Yüksel. I build things for the web.
+            </div>
+            <p className="para">
+              Since a while, I've been working remotely as a full time front-end
+              developer (VueJS), although my presonal preference goes with
+              ReactJs
+            </p>
+            <p className="para">
+              I'm a self-taught programmer, my BA is in Comp. Lit. (I studied it
+              partly in Turkey and Budapest/Hungary).
+            </p>
+            <p className="para">
+              I did my MA in Semiotics in Tartu/Estonia and Bordeaux/France.{" "}
+            </p>
+            <p className="para">I'm fluent in English, French, and Turkish.</p>
+            <p className="para">
+              As a lifelong learner, these days I'm interested heavily with the
+              web3 space, studying Solidity daily.
+            </p>
+          </div>
         </div>
         <div className="techStack">
           <h2 className="title">My Tech Stack</h2>
         </div>
       </div>
-      <div className="glass"></div>
     </div>
   );
 };
